13da45d509af22c61e58036836668fe3b2bf8690
tatka18 <tatka19871218@gmail.com>
2021-10-28 22:37:43 +0300

    execute() method with signature type String has been implemented in AuthorizationCommand class

--- src/by/epamtc/library/controller/impl/AuthorizationCommand.java
+++ src/by/epamtc/library/controller/impl/AuthorizationCommand.java
@@ -1,4 +1,29 @@
 package by.epamtc.library.controller.impl;
 
-public class AuthorizationCommand {
+import by.epamtc.library.controller.Command;
+import by.epamtc.library.model.User;
+import by.epamtc.library.service.ServiceException;
+import by.epamtc.library.service.ServiceProvider;
+
+public class AuthorizationCommand implements Command {
+    @Override
+    public String execute(String request) {
+        String []params = request.split(" ", 2);
+        String login;
+        String password;
+        String divider = "=";
+
+        login = params[0].substring(params[0].indexOf(divider) + 1);
+        password = params[1].substring(params[1].indexOf(divider) + 1);
+
+        String result = "";
+        try{
+            User user = ServiceProvider.getInstance().getUserService().authorization(login, password);
+            System.out.println();
+            result = user.getFirstName() + " " + user.getLastName() + ", welcome to our library!";
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
