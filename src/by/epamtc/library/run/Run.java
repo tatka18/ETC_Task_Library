58588345152a38a81135fecba8e4e7a127b8fa8f
tatka18 <tatka19871218@gmail.com>
2021-10-28 22:42:19 +0300

    method main() was supplemented with authorization request

--- src/by/epamtc/library/run/Run.java
+++ src/by/epamtc/library/run/Run.java
@@ -1,7 +1,18 @@
 package by.epamtc.library.run;
 
+import by.epamtc.library.controller.Controller;
+import by.epamtc.library.view.PrintResult;
+
 public class Run {
     public static void main(String[] args) {
+        Controller controller = new Controller();
+
+        String request;
+        String response;
+
+        request = "authorization login=asd password=asdfg";
+        response = controller.action(request);
+        PrintResult.printString(response);
 
     }
 }
