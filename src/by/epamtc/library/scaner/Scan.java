3b06e62da8cb7e4dd8616961a7c654506a1ad261
tatka18 <tatka19871218@gmail.com>
2021-10-31 23:34:57 +0300

    data parameters from ListData interface constant have been inserted

--- src/by/epamtc/library/scaner/Scan.java
+++ src/by/epamtc/library/scaner/Scan.java
@@ -1,6 +1,7 @@
 package by.epamtc.library.scaner;
 
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -8,14 +9,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Scan {
-    public static List<String> readTextFromFile() throws IOException {
-        String fileName = "src/by/epamtc/resources/users.txt";
-        Path path = Paths.get(fileName);
-        Scanner scanner = new Scanner(path);
-
+    public static List<String> readTextFromFile(String data) throws IOException {
         List<String> stringList = new ArrayList<>();
-        while (scanner.hasNextLine()) {
-            stringList.add(scanner.nextLine());
+        try{
+//            String fileName = "src/by/epamtc/resources/users.txt";
+            Path path = Paths.get(data);
+            Scanner scanner = new Scanner(path);
+
+            while (scanner.hasNextLine()) {
+                stringList.add(scanner.nextLine());
+            }
+        }catch (NoSuchFileException e){
+            throw new IOException("problem with getting data from file", e);
         }
         return stringList;
     }
