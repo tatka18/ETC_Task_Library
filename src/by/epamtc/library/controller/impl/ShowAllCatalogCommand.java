c4449ae1ceb7f7d5e0a0d2b545c221622bcf48de
tatka18 <tatka19871218@gmail.com>
2021-10-31 23:44:59 +0300

    method execute has been implemented in ShowAllCatalogCommand class

--- src/by/epamtc/library/controller/impl/ShowAllCatalogCommand.java
+++ src/by/epamtc/library/controller/impl/ShowAllCatalogCommand.java
@@ -1,4 +1,22 @@
 package by.epamtc.library.controller.impl;
 
-public class ShowAllCatalogCommand {
+import by.epamtc.library.controller.Command;
+import by.epamtc.library.model.Book;
+import by.epamtc.library.service.ServiceException;
+import by.epamtc.library.service.ServiceProvider;
+import by.epamtc.library.view.PrintResult;
+
+import java.util.List;
+
+public class ShowAllCatalogCommand implements Command {
+    @Override
+    public String execute(String request) {
+        try{
+            List<Book> bookList = ServiceProvider.getInstance().getBookService().findAllBooks();
+            PrintResult.printBookList(bookList);
+        }catch (ServiceException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
