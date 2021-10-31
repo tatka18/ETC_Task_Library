80b4f34d0d3b5de6b8352da2596587a5fe3ac6a0
tatka18 <tatka19871218@gmail.com>
2021-10-31 20:58:41 +0300

    DaoException and ServiceException classes have been supplemented by exception constructors

--- src/by/epamtc/library/dao/DaoException.java
+++ src/by/epamtc/library/dao/DaoException.java
@@ -3,7 +3,18 @@ package by.epamtc.library.dao;
 public class DaoException extends Exception {
     private static final long serialVersionUID = 1L;
 
-        public DaoException(String message, Exception e) {
-            super(message, e);
-        }
+    public DaoException() {
+        super();
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(Exception e) {
+        super(e);
+    }
+    public DaoException(String message, Exception e) {
+        super(message, e);
+    }
 }
--- src/by/epamtc/library/service/ServiceException.java
+++ src/by/epamtc/library/service/ServiceException.java
@@ -3,6 +3,17 @@ package by.epamtc.library.service;
 public class ServiceException extends Exception{
 
     private static final long serialVersionUID = 1L;
+    public ServiceException() {
+        super();
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(Exception e) {
+        super(e);
+    }
 
     public ServiceException(String message, Exception e) {
         super(message, e);
