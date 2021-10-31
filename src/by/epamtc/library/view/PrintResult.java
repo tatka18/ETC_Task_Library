31c4ee594ef7f64887a185e796b22ec38cd5f069
tatka18 <tatka19871218@gmail.com>
2021-10-31 23:51:10 +0300

    method printBookList has been added

--- src/by/epamtc/library/view/PrintResult.java
+++ src/by/epamtc/library/view/PrintResult.java
@@ -1,7 +1,17 @@
 package by.epamtc.library.view;
 
+import by.epamtc.library.model.Book;
+
+import java.util.List;
+
 public class PrintResult {
     public static void printString(String string){
         System.out.println(string);
     }
+    public static void printBookList(List<Book> bookList){
+        for (Book book: bookList){
+            System.out.println(book.getBookName() + " " + book.getAuthor());
+        }
+    }
+
 }
