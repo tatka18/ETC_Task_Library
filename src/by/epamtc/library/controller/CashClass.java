51b3a8971a2f40626ba51dd47426dfb99a706e25
tatka18 <tatka19871218@gmail.com>
2021-11-05 12:40:44 +0300

    cashClass class has been added for saving user role parameter

---
+++ src/by/epamtc/library/controller/CashClass.java
@@ -0,0 +1,12 @@
+package by.epamtc.library.controller;
+
+public class CashClass {
+    private static String userRole;
+
+    public static void setCashedUserRole(String userRole){
+        CashClass.userRole = userRole;
+    }
+    public static String getCashedUserRole(){
+        return userRole;
+    }
+}
